65928af66015f437d596c0289131e5bad732b6d6
vojtabouragtp <semsamsum2@gmail.com>
2026-09-09 17:21:06 +0200

    Expose Reality Processor menu item in both Library and File Plug-in Extras

--- LightroomPlugin/RealityProcessor.lrplugin/Info.lua
+++ LightroomPlugin/RealityProcessor.lrplugin/Info.lua
@@ -3,8 +3,18 @@ return {
     LrSdkMinimumVersion = 3.0,
     LrToolkitIdentifier = 'com.vojtaboura.realityprocessor',
     LrPluginName = 'Reality Processor',
+    VERSION = { major = 1, minor = 1, revision = 0, build = 0 },
 
+    -- Library -> Plug-in Extras
     LrLibraryMenuItems = {
+        {
+            title = 'Reality Processor: Načíst HDR frontu',
+            file = 'LoadHDRQueue.lua',
+        },
+    },
+
+    -- Fallback: File -> Plug-in Extras
+    LrExportMenuItems = {
         {
             title = 'Reality Processor: Načíst HDR frontu',
             file = 'LoadHDRQueue.lua',
