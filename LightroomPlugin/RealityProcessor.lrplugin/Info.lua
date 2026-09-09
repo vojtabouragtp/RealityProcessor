8c1a61f14ea108b5f4c252650b4aa7dfa8123619
vojtabouragtp <semsamsum2@gmail.com>
2026-09-09 19:08:45 +0200

    Bump Lightroom bridge plugin to 1.3

--- LightroomPlugin/RealityProcessor.lrplugin/Info.lua
+++ LightroomPlugin/RealityProcessor.lrplugin/Info.lua
@@ -3,7 +3,7 @@ return {
     LrSdkMinimumVersion = 3.0,
     LrToolkitIdentifier = 'com.vojtaboura.realityprocessor',
     LrPluginName = 'Reality Processor',
-    VERSION = { major = 1, minor = 2, revision = 0, build = 0 },
+    VERSION = { major = 1, minor = 3, revision = 0, build = 0 },
 
     LrInitPlugin = 'Init.lua',
 
