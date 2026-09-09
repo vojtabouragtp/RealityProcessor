e9dd6e43f5aa67655a0a02bcae11bddeeab5c53a
vojtabouragtp <semsamsum2@gmail.com>
2026-09-09 14:52:21 +0200

    Add Lightroom Classic plugin skeleton

---
+++ LightroomPlugin/RealityProcessor.lrplugin/Info.lua
@@ -0,0 +1,13 @@
+return {
+    LrSdkVersion = 12.0,
+    LrSdkMinimumVersion = 3.0,
+    LrToolkitIdentifier = 'com.vojtaboura.realityprocessor',
+    LrPluginName = 'Reality Processor',
+
+    LrLibraryMenuItems = {
+        {
+            title = 'Reality Processor: Načíst HDR frontu',
+            file = 'LoadHDRQueue.lua',
+        },
+    },
+}
