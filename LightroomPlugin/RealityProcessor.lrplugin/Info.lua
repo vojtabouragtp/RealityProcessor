3ee6a6d89aa190ea8d1e078b01dcaca375e095c3
vojtabouragtp <semsamsum2@gmail.com>
2026-09-10 10:51:21 +0200

    Bump Lightroom plugin version to 1.7

--- LightroomPlugin/RealityProcessor.lrplugin/Info.lua
+++ LightroomPlugin/RealityProcessor.lrplugin/Info.lua
@@ -3,10 +3,8 @@ return {
     LrSdkMinimumVersion = 3.0,
     LrToolkitIdentifier = 'com.vojtaboura.realityprocessor',
     LrPluginName = 'Reality Processor',
-    VERSION = { major = 1, minor = 6, revision = 0, build = 0 },
+    VERSION = { major = 1, minor = 7, revision = 0, build = 0 },
 
-    -- Používáme existující LoadHDRQueue.lua i jako init skript.
-    -- Tím se vyhneme problému, kdy Lightroom lokálně neviděl nově přidané Init/PluginInit soubory.
     LrInitPlugin = 'LoadHDRQueue.lua',
     LrForceInitPlugin = true,
 
